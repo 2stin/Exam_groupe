dc724dbf9188db2d3be3304e8767d130ce7dca7e
cbaudequin <cbaudequin@gmail.com>
2024-01-15 15:05:06 +0100

    premier_test_changer_joueur

--- test/test_noyau.py
+++ test/test_noyau.py
@@ -1,3 +1,11 @@
-from gui import *
+from noyau import *
 import pytest
+
+def test_changer_joueur():
+    assert changer_joueur(0) == 1
+    assert changer_joueur(1) == 0
+    assert changer_joueur(28) == 1
+    assert changer_joueur(27) == 0
+    assert changer_joueur(-28) == 1
+    assert changer_joueur(-27) == 0
 
